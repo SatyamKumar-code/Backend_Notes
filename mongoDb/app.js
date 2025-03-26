82e2e741b3dc0b5d3f900e16a022612c8d13fa16
SatyamKumar-code <satyamwebsite1@gmail.com>
2025-03-26 22:06:55 +0530

    uptade in mongodb_file

--- mongoDb/app.js
+++ mongoDb/app.js
@@ -28,5 +28,17 @@ app.post("/create",async ( req, res ) => {
     })
     res.redirect("/read")
 })
-
+app.get("/edit/:userid", async ( req, res ) => {
+    let user = await userModel.findOne({_id: req.params.userid});
+    res.render("edit", {user});
+})
+app.post("/update/:userid", async ( req, res ) => {
+    let { name, email, image } = req.body;
+    let updatedUser = await userModel.findOneAndUpdate({_id: req.params.userid}, {
+        name,
+        email,
+        image
+    }, {new: true});
+    res.redirect("/read")
+})
 app.listen(3000);
