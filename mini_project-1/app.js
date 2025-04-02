685c724e15a28d228aeb3b0352ae540457fb693c
SatyamKumar-code <satyamkumarsing59@gmail.com>
2025-04-02 23:44:46 +0530

    Add mini project

---
+++ mini_project-1/app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+
+const userModel = require('./model/userModel');
+const postModel = require('./model/postModel');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const jwt = require('jsonwebtoken');
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.get("/profile", isLoggedIn, async (req, res) => {
+    console.log(req.user);
+    
+    res.render("login");
+})
+
+app.post("/register", async (req, res) => {
+    let { username, name, age, email, password } = req.body;
+    let user = await userModel.findOne({ email });
+    if(user) {
+        return res.status(500).send("user already exists");
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let user = await userModel.create({
+                username,
+                name,
+                age,
+                email,
+                password: hash
+            });
+
+            let token = jwt.sign({email: email, userid: user._id}, "secret");
+            res.cookie("token", token);
+            res.send(user);
+        } )
+    })
+})
+
+app.post("/login", async (req, res) => {
+    let { email, password } = req.body;
+    let user = await userModel.findOne({ email });
+    if(!email || !password) {
+        return res.redirect("/login");
+    }
+    if(!user) {
+        return res.status(500).json({ message: "user not found" }).send("something went wrong");
+    }
+
+    bcrypt.compare(password, user.password, (err, result) => {
+        if(result){
+            let token = jwt.sign({email: email, user: user._id}, "secret");
+            res.cookie("token", token);
+            res.status(200).send("login successful");
+        } else {
+            res.redirect("/login");
+        }
+    })
+})
+
+app.get("/logout", (req, res) => {
+    res.clearCookie("token");
+    res.redirect("/login");
+})
+
+function isLoggedIn(req, res, next) {
+    if(!req.cookies.token) {
+        return res.send("You must be logged in");
+    }
+    
+    try {
+        let data = jwt.verify(req.cookies.token, "secret");
+        req.user = data;
+        next();
+    } catch(error) {
+        return res.send("Invalid token, please login again");
+    }
+}
+    
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
---
+++ mini_project-1/model/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/mini_project" );
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 60
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /.+\@.+\..+/ // Basic email validation
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4,
+    },
+    post: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "post"
+        }
+    ]
+})
+
+module.exports = mongoose.model("user", userSchema);
